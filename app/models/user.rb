9353b32c96104279302cc64b8a4d30b5beb71589
JoaquinFernandezC <jafernandez1@miuandes.cl>
2018-05-04 22:12:19 -0300

    added Devise model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,6 @@
 class User < ApplicationRecord
-  has_many :product
-  has_many :wish_list
-  has_many :fund
-  has_many :bank_datum
-
-  validates :email,
-            presence: true,
-            format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
-  validates :password,
-            format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ ,
-                                message: "Password must be between 8 to 12 alphanumeric characters"}
-  validates :name,
-            presence: true,
-            numericality: false
-  validates :isAdmin,
-            presence: true
-  validates :lastLogin,
-            allow_blank: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
